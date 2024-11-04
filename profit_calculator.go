200e79ebdea563ed605729d3139362db4de6a634
RafaelFRF <rafaelfiorini.rf@gmail.com>
2024-11-04 15:26:11 -0300

    Feat: implement multiple functions

--- profit_calculator.go
+++ profit_calculator.go
@@ -5,22 +5,37 @@ import (
 )
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
 
-	fmt.Print("Revenue($): ")
-	fmt.Scan(&revenue)
-	fmt.Print("Expenses($): ")
-	fmt.Scan(&expenses)
-	fmt.Print("Tax Rate(%): ")
-	fmt.Scan(&taxRate)
+	// fmt.Print("Revenue($): ")
+	// fmt.Scan(&revenue)
+	// fmt.Print("Expenses($): ")
+	// fmt.Scan(&expenses)
+	// fmt.Print("Tax Rate(%): ")
+	// fmt.Scan(&taxRate)
+	revenue := getUserValue("Revenue($): ")
+	expenses := getUserValue("Expenses($): ")
+	taxRate := getUserValue("Tax Rate(%): ")
 
+	// earningsBeforeTax := revenue - expenses
+	// profit := earningsBeforeTax * (1 - taxRate/100) // Earnings After Tax
+	// ratio := earningsBeforeTax / profit
+	earningsBeforeTax, profit, ratio := doMaths(revenue, expenses, taxRate)
+
+	fmt.Printf("%.1f\n", earningsBeforeTax)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f", ratio)
+}
+
+func getUserValue(infoText string) float64 {
+	var userValue float64
+	fmt.Print(infoText)
+	fmt.Scan(&userValue)
+	return userValue
+}
+
+func doMaths(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	earningsBeforeTax := revenue - expenses
 	profit := earningsBeforeTax * (1 - taxRate/100) // Earnings After Tax
 	ratio := earningsBeforeTax / profit
-
-	fmt.Println(earningsBeforeTax)
-	fmt.Println(profit)
-	fmt.Println(ratio)
+	return earningsBeforeTax, profit, ratio
 }
